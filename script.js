81d75c4b229f70dc26086e4ff1e4a7627da3137d
Tushar Srivastava <tusharsrivastava2710@gmail.com>
2026-03-26 00:49:43 +0530

    Added basic test UI with static questions

---
+++ script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "What is CPU?",
+        options: ["Brain", "Memory", "Storage", "None"],
+        answer: "Brain"
+    },
+    {
+        question: "What is RAM?",
+        options: ["Temporary Memory", "Permanent", "CPU", "None"],
+        answer: "Temporary Memory"
+    }
+];
+
+let currentIndex = 0;
+
+function loadQuestion() {
+    const q = questions[currentIndex];
+
+    document.getElementById("question").innerText = q.question;
+
+    const optionsDiv = document.getElementById("options");
+    optionsDiv.innerHTML = "";
+
+    q.options.forEach(option => {
+        const btn = document.createElement("button");
+        btn.innerText = option;
+
+        btn.onclick = () => checkAnswer(option);
+
+        optionsDiv.appendChild(btn);
+    });
+}
+
+function checkAnswer(selected) {
+    if (selected === questions[currentIndex].answer) {
+        alert("Correct!");
+    } else {
+        alert("Wrong!");
+    }
+}
+
+function nextQuestion() {
+    currentIndex++;
+
+    if (currentIndex < questions.length) {
+        loadQuestion();
+    } else {
+        alert("Test Finished!");
+    }
+}
+
+loadQuestion();
